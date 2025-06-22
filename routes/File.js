4b85af1031d9fd57c87a14e97abff3669caaea2e
Kaikaio <1035759181@qq.com>
2025-06-22 11:23:37 +0800

    fix: 环境变量启动兼容Windows平台，DotEnv无法在Windows使用，先注释相关文件功能

--- routes/File.js
+++ routes/File.js
@@ -8,8 +8,10 @@ dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
 
 const OSSClient = new OSS({
   // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
-  accessKeyId: process.env.OSS_ACCESS_KEY_ID,
-  accessKeySecret: process.env.OSS_ACCESS_KEY_SECRET,
+  // accessKeyId: process.env.OSS_ACCESS_KEY_ID,
+  // accessKeySecret: process.env.OSS_ACCESS_KEY_SECRET,
+  accessKeyId: 'xx',
+  accessKeySecret: 'xx',
   // yourRegion填写Bucket所在地域。以华东1（杭州）为例，Region填写为oss-cn-hangzhou。
   region: 'oss-cn-beijing',
   authorizationV4: true,
