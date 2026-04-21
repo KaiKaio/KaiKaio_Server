82fbbf5c2dbe6ffdb7b916fcd29108937d25af3d
Kaiwei <kaikaiano4@gmail.com>
2026-04-21 17:51:12 +0800

    fix: 解决内存占用过高的问题

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,9 @@ module.exports = {
     {
       name: 'blogserver',
       script: 'app.js',
-      instances: 'max',
-      exec_mode: 'cluster',
+      instances: 1, // 单实例足够低频调用场景 无需 'max'
+      exec_mode: 'fork', // 单进程模式，无需集群（cluster）
+      max_memory_restart: '200M', // 内存超限自动重启保护
       env: {
         NODE_ENV: 'production'
       }
